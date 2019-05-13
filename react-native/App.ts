2a3da7d92c5f7d43ef631a4e79f671699b1785ca
Grayson Ricketts <graysonricketts@graysons-mini.attlocal.net>
2019-05-13 16:52:23 -0500

    App moved to TypeScript

---
+++ react-native/App.ts
@@ -0,0 +1,55 @@
+/**
+ * App starting point
+ */
+
+import { 
+  createStackNavigator, 
+  createAppContainer
+} from 'react-navigation';
+import WelcomeView from './containers/WelcomeView';
+import ProfileView from './containers/ProfileView';
+import PreviousSessionsView from './containers/PreviousSessionsView';
+import StatsView from './containers/StatsView';
+import TrainingSessionView from './containers/TrainingSessionView';
+import ReportBugView from './containers/profile/ReportBugView';
+import SendSuggestionView from './containers/profile/SendSuggestionView';
+import SettingsView from './containers/profile/SettingsView';
+import AboutView from './containers/profile/AboutView';
+
+
+const MainStack = createStackNavigator(
+  {
+    Welcome: {
+      screen: WelcomeView
+    },
+    TrainingSession: {
+      screen: TrainingSessionView
+    },
+    PreviousSessions: {
+      screen: PreviousSessionsView
+    },
+    Stats: {
+      screen: StatsView
+    },
+    Profile: {
+      screen: ProfileView
+    },
+    ReportBug: {
+      screen: ReportBugView
+    },
+    SendSuggestion: {
+      screen: SendSuggestionView
+    },
+    Settings: {
+      screen: SettingsView
+    },
+    About: {
+      screen: AboutView
+    }
+  },
+  {
+    initialRouteName: 'Welcome',
+  }
+);
+
+export default createAppContainer(MainStack);
