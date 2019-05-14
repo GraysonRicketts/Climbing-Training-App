c8e7531331c660c36c998a36c10b65744a37e811
Grayson Ricketts <graysonricketts@graysons-mini.attlocal.net>
2019-05-14 13:26:16 -0500

    rebase util

--- react-native/util/Climbs.d.ts
+++ react-native/util/Climbs.d.ts
@@ -21,26 +21,9 @@ type ClimbingSession = {
     title?: string
 }
 
-/**
- * Async data is stored in a two-dimensional array. It's an an array of climb
- * data arrays. This has two columns. The first column is string date the session 
- * happened on. The second column is a stringified JSON
- * of the the climbs that session.
- * 
- * e.g. 
- * [ 
- *  [ 'day1', 'JSON climb data' ],
- *  [ 'day2', 'JSON climb data' ],
- *  [ 'day3', 'JSON climb data' ],
- *  [ 'day4', 'JSON climb data' ]
- * ]
- */
-type AsyncSessionStorage = [string, string | null][]; 
-
 export {
     Difficulty,
     Route,
     Climb,
-    ClimbingSession,
-    AsyncSessionStorage
+    ClimbingSession
 };
